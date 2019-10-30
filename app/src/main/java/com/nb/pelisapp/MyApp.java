6bac77d20028549fbc0a54073e646c0b8a9f2ddd
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-30 18:54:59 -0500

    Estableciendo clase Application

---
+++ app/src/main/java/com/nb/pelisapp/MyApp.java
@@ -0,0 +1,23 @@
+package com.nb.pelisapp;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApp extends Application {
+
+    private static MyApp sInstance;
+
+    public static MyApp getInstance() {
+        return sInstance;
+    }
+
+    public static Context getContext() {
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        sInstance = this;
+        super.onCreate();
+    }
+}
